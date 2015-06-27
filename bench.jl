391a5629567dbe6de0501ad1f40552690c88e18f
Kyle Barbary <kylebarbary@gmail.com>
2015-06-27 01:21:24 -0400

    add sum_circann() function

--- bench.jl
+++ bench.jl
@@ -6,6 +6,8 @@ data = ones(10, 10)
 error = 0.1 * ones(10, 10)
 mask = falses(10, 10)
 
-@timeit sum_circle(data, nothing, 0.1, 5.0, 5.0, 3.0; maskthresh=1)
-@timeit sum_circle(data, mask, error, 5.0, 5.0, 3.0)
-@timeit sum_circle(data, 5.0, 5.0, 3.0)
+@timeit sum_circle(data, 5.0, 5.0, 4.0)
+@timeit sum_circle(data, 5.0, 5.0, 4.0; mask=error, error=error, maskthresh=0.2)
+
+@timeit sum_circann(data, 5.0, 5.0, 2.0, 4.0)
+@timeit sum_circann(data, 5.0, 5.0, 2.0, 4.0; mask=error, error=error, maskthresh=0.2)
--- src/AperturePhotometry.jl
+++ src/AperturePhotometry.jl
@@ -1,51 +1,10 @@
 module AperturePhotometry
 
-export sum_circle
+export sum_circle, sum_circann
 
 using Compat
 
 include("overlap.jl")
-
-function sum_circle{T}(data::Array{T, 2},
-                       x::Real, y::Real, r::Real)
-
-    r2 = r * r
-
-    # Internal and external radius of the oversampled annulus
-    rin = r - (sqrt(2.0) / 2.0)
-    rin2 = rin > 0.0 ? rin * rin : 0.0
-    rout = r + (sqrt(2.0) / 2.0)
-    rout2 = rout * rout
-
-    # Box of pixel positions to consider
-    xmin = round(Int, x - r)
-    xmax = round(Int, x + r)
-    ymin = round(Int, y - r)
-    ymax = round(Int, y + r)
-
-    sum = 0.0
-    var = 0.0
-    area = 0.0
-    pixarea = 0.0
-
-    for iy in ymin:ymax
-        for ix in xmin:xmax
-            dx = ix - x
-            dy = iy - y
-            ir2 = dx*dx + dy*dy
-
-            if ir2 < rout2
-                pixarea = (ir2 < rin2 ? 1.0 :
-                           circoverlap(dx-0.5, dy-0.5, dx+0.5, dy+0.5, r))
-                area += pixarea
-                sum += data[x, y] * pixarea
-            end
-        end
-    end
-
-    return sum
-end
-
 
 # -----------------------------------------------------------------------------
 
@@ -58,72 +17,117 @@ function circbounds(data, x, y, r)
     return xmin, xmax, ymin, ymax
 end
 
-# helper functions
-ismasked{T}(mask::AbstractArray{T, 2}, x, y, thresh::T) = mask[x, y] > thresh
-ismasked(mask::Nothing, x, y, thresh) = false
+# Internal and external radius^2 of the oversampled annulus
+function oversampann(r)
+    rin = r - sqrt(2.0) / 2.0
+    rout = r + sqrt(2.0) / 2.0
 
-type StDev end
-getvariance(error::Nothing, x, y, ::Type{StDev}) = 0.0
-getvariance(error::Real, x, y, ::Type{StDev}) =
-    @compat Float64(error) * Float64(error)
-getvariance{T}(error::AbstractArray{T, 2}, x, y, ::Type{StDev}) =
+    return (rin < 0.0 ? 0.0 : rin*rin), rout*rout
+end
+
+ismasked(mask::Nothing, x, y, thresh) = false
+ismasked{T<:Real}(mask::AbstractArray{T, 2}, x, y, thresh::T) =
+    mask[x, y] > thresh
+
+getvar(error::Nothing, x, y) = 0.0
+getvar(error::Real, x, y) = @compat Float64(error) * Float64(error)
+getvar{T<:Real}(error::AbstractArray{T, 2}, x, y) =
     @compat Float64(error[x, y]) * Float64(error[x, y])
 
-# 
-# mask can be nothing or a 2-d array
-# error can be nothing, a Real, or a 2-d array
 
-function sum_circle{T}(data::AbstractArray{T, 2}, mask, error,
-                       x::Real, y::Real, r::Real; maskthresh=false, gain=1.0)
+# mask : Nothing or AbstractArray{T, 2}
+# error : Nothing, Real, AbstractArray{T, 2}
+function sum_circle{T<:Real}(data::AbstractArray{T, 2}, x::Real, y::Real, r::Real;
+                             mask=nothing, error=nothing, maskthresh=false, gain=1.0)
 
     mthresh = convert(eltype(mask), maskthresh)
     r2 = r * r
-
-    # Internal and external radius of the oversampled annulus
-    rin = r - sqrt(2.0) / 2.0
-    rin2 = rin > 0.0 ? rin * rin : 0.0
-    rout = r + sqrt(2.0) / 2.0
-    rout2 = rout * rout
-
-    # Box of pixel positions to consider
+    rin2, rout2 = oversampann(r)
     xmin, xmax, ymin, ymax = circbounds(data, x, y, r)
 
-    sum = 0.0
-    var = 0.0
-    area = 0.0
-    maskarea = 0.0
-    pixarea = 0.0
+    sum, var, area, maskarea, pixarea = 0.0, 0.0, 0.0, 0.0, 0.0
+    
+    for ypix in ymin:ymax
+        for xpix in xmin:xmax
+            dx = xpix - x
+            dy = ypix - y
+            rpix2 = dx*dx + dy*dy
 
-    for iy in ymin:ymax
-        for ix in xmin:xmax
-            dx = ix - x
-            dy = iy - y
-            ir2 = dx*dx + dy*dy
-
-            if ir2 < rout2
-                pixarea = (ir2 < rin2 ? 1.0 :
+            if rpix2 < rout2
+                pixarea = (rpix2 < rin2 ? 1.0 :
                            circoverlap(dx-0.5, dy-0.5, dx+0.5, dy+0.5, r))
-                if ismasked(mask, ix, iy, mthresh)
+                if ismasked(mask, xpix, ypix, mthresh)
                     maskarea += pixarea
                 else
+                    sum += pixarea * data[xpix, ypix]
+                    var += pixarea * getvar(error, xpix, ypix)
                     area += pixarea
-                    sum += data[ix, iy] * pixarea
-                    var += pixarea * getvariance(error, ix, iy, StDev)
                 end
             end
         end
-    end
-
-    # correct masked pixels to average value in non-masked pixels.
-    if area > 0.0
-        sum *= (maskarea + area) / area
     end
 
     if gain > 0.0 && sum > 0.0
         var += sum / gain
     end
 
+    if area > 0.0
+        factor = 1.0 + maskarea / area
+        sum *= factor
+        var *= factor
+    end
+
     return sum, sqrt(var)
 end
+
+function sum_circann{T<:Real}(data::AbstractArray{T, 2}, x::Real, y::Real,
+                              rin::Real, rout::Real; mask=nothing,
+                              error=nothing, maskthresh=false, gain=1.0)
+
+    mthresh = convert(eltype(mask), maskthresh)
+    rin2 = rin * rin
+    rinin2, rinout2 = oversampann(rin)
+    routin2, routout2 = oversampann(rout)
+    xmin, xmax, ymin, ymax = circbounds(data, x, y, rout)
+
+    sum, var, area, maskarea, pixarea = 0.0, 0.0, 0.0, 0.0, 0.0
+    
+    for ypix in ymin:ymax
+        for xpix in xmin:xmax
+            dx = xpix - x
+            dy = ypix - y
+            rpix2 = dx*dx + dy*dy
+
+            if rpix2 < routout2 && rpix2 > rinin2
+                pixarea = (rpix2 < routin2 ? 1.0 :
+                           circoverlap(dx-0.5, dy-0.5, dx+0.5, dy+0.5, rout))
+                if rpix2 < rinout2
+                    pixarea -= circoverlap(dx-0.5, dy-0.5, dx+0.5, dy+0.5, rin)
+                end
+
+                if ismasked(mask, xpix, ypix, mthresh)
+                    maskarea += pixarea
+                else
+                    sum += pixarea * data[xpix, ypix]
+                    var += pixarea * getvar(error, xpix, ypix)
+                    area += pixarea
+                end
+            end
+        end
+    end
+
+    if gain > 0.0 && sum > 0.0
+        var += sum / gain
+    end
+
+    if area > 0.0
+        factor = 1.0 + maskarea / area
+        sum *= factor
+        var *= factor
+    end
+
+    return sum, sqrt(var)
+end
+
 
 end # module
